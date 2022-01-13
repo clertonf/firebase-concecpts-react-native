2f84bfe4b9598785041d4286db59bf4f015318e8
clertonf <clerton.filho7@gmail.com>
2022-01-12 22:16:46 -0300

    displaying photos added in firebase

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,37 +1,59 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+import storage from "@react-native-firebase/storage";
 
-import { Container, PhotoInfo } from './styles';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { Container, PhotoInfo } from "./styles";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
+import { File, FileProps } from "../../components/File";
 
 export function Receipts() {
+  const [photos, setPhotos] = useState<FileProps[]>([]);
+  const [photoSelected, setPhotoSelected] = useState("");
+
+  async function handleShowImage(path: string) {
+    const urlImage = await storage().ref(path).getDownloadURL();
+    setPhotoSelected(urlImage);
+  }
+
+  useEffect(() => {
+    storage()
+      .ref("/Images")
+      .list()
+      .then((result) => {
+        const files: FileProps[] = [];
+
+        result.items.forEach((file) => {
+          files.push({
+            name: file.name,
+            path: file.fullPath,
+          });
+        });
+        setPhotos(files);
+      });
+  }, []);
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
-      <Photo uri="" />
+      <Photo uri={photoSelected} />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>Informações da foto</PhotoInfo>
 
       <FlatList
-        data={photosData}
-        keyExtractor={item => item.name}
+        data={photos}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
           <File
             data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
+            onShow={() => handleShowImage(item.path)}
+            onDelete={() => {}}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
-        style={{ width: '100%', padding: 24 }}
+        style={{ width: "100%", padding: 24 }}
       />
     </Container>
   );
